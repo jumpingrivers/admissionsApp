0ede881b419e4e205f62c70b799c67843fac9931
Mandy Norrbo <mandy@jumpingrivers.com>
2022-07-26 09:40:37 +0000

    App skeleton

---
+++ R/app_server.R
@@ -0,0 +1,11 @@
+#' The application server-side
+#'
+#' @param input,output,session Internal parameters for {shiny}.
+#'     DO NOT REMOVE.
+#' @import shiny
+#' @noRd
+app_server <- function(input, output, session) {
+  mod_line_server("line_1")
+  mod_sunburst_server("sunburst_1")
+  mod_help_server("help_1")
+}
---
+++ R/mod_help.R
@@ -0,0 +1,26 @@
+#' help UI Function
+#'
+#' @description A shiny Module.
+#'
+#' @param id,input,output,session Internal parameters for {shiny}.
+#'
+#' @noRd
+#'
+#' @importFrom shiny NS tagList
+mod_help_ui <- function(id){
+  ns <- NS(id)
+  shiny::tabPanel(
+    "Help",
+    "Awaiting content"
+  )
+}
+
+#' help Server Functions
+#'
+#' @noRd
+mod_help_server <- function(id){
+  moduleServer( id, function(input, output, session){
+    ns <- session$ns
+
+  })
+}
