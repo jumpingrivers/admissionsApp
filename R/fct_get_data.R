51d6e26a1efe4b411663c4e063dc79d17277aa3a
Russ Hyde <russ@jumpingrivers.com>
2023-05-25 17:05:12 +0100

    refac: style(fct_get_data)

--- R/fct_get_data.R
+++ R/fct_get_data.R
@@ -8,23 +8,21 @@
 #'
 #' @return   data.frame containing the daily enrollment data.
 
-get_daily_enrollment = function(method="from_rds") {
-
+get_daily_enrollment <- function(method = "from_rds") {
   if (method == "from_sql") {
-    daily_enrollment_df <- utHelpR::get_data_from_sql_file("daily_enrollment.sql", dsn="edify", context="shiny")
-  }
-  else if (method == "from_rds") {
-   daily_enrollment_df <- readRDS(here::here("inst", "app", "fake_data", "daily_enrollment.rds")) %>%
-    dplyr::mutate( year = as.character( sample(1978:2022, length(term_id), replace = TRUE) ),
-                   season = as.character( sample(c("Spring", "Fall", "Summer"), length(term_id), replace = TRUE) ))
-  }
-  else if (method == "from_pin") {
-    pin_user = get_pin_user()
-    board_rsc = pins::board_rsconnect()
-    enrollment_path = glue::glue("{pin_user}/daily_enrollment_pin")
-    daily_enrollment_df = pins::pin_read(board_rsc, enrollment_path)
-  }
-  else {
+    daily_enrollment_df <- utHelpR::get_data_from_sql_file("daily_enrollment.sql", dsn = "edify", context = "shiny")
+  } else if (method == "from_rds") {
+    daily_enrollment_df <- readRDS(here::here("inst", "app", "fake_data", "daily_enrollment.rds")) %>%
+      dplyr::mutate(
+        year = as.character(sample(1978:2022, length(term_id), replace = TRUE)),
+        season = as.character(sample(c("Spring", "Fall", "Summer"), length(term_id), replace = TRUE))
+      )
+  } else if (method == "from_pin") {
+    pin_user <- get_pin_user()
+    board_rsc <- pins::board_rsconnect()
+    enrollment_path <- glue::glue("{pin_user}/daily_enrollment_pin")
+    daily_enrollment_df <- pins::pin_read(board_rsc, enrollment_path)
+  } else {
     stop("Method for gathering daily enrollment data is not defined.")
   }
   return(daily_enrollment_df)
@@ -35,8 +33,8 @@ get_daily_enrollment = function(method="from_rds") {
 #'
 #' Read environment variable which sets
 #' which RStudio Connect account pinned data is read from
-get_pin_user = function() {
-  pin_user = Sys.getenv("PIN_USER")
+get_pin_user <- function() {
+  pin_user <- Sys.getenv("PIN_USER")
   if (pin_user == "") {
     cli::cli_alert_warning("Ensure you have an environment variable called PIN_USER")
     cli::cli_alert_warning("PIN_USER should be the user where data pins are uploaded")
