be284b649ac1ba0926bf850664a601b0d238380e
Russ Hyde <russ@jumpingrivers.com>
2023-08-08 10:53:45 +0100

    refac: style(fct_style_utils)

--- R/fct_style_utils.R
+++ R/fct_style_utils.R
@@ -1,7 +1,7 @@
 #' Create title logo
 #'
 #' Returns UT logo with correct dimensions for app title.
-title_logo = function() {
+title_logo <- function() {
   shiny::div(
     style = "text-align: justify; width:150;",
     shiny::tags$img(
@@ -15,37 +15,42 @@ title_logo = function() {
       alt = "UT Data"
     ),
     shiny::h3("Admissions dashboard",
-              style = "position: absolute; right: 1%; top: 0%; margin-top: 10px;")
+      style = "position: absolute; right: 1%; top: 0%; margin-top: 10px;"
+    )
   )
 }
 
 #' Create custom litera theme
 #'
 #' Modify litera theme with custom fonts and colours.
-litera_theme = function() {
+litera_theme <- function() {
   bslib::bs_theme(
     bootswatch = "litera",
     bg = "#FFFFFF", fg = "#000",
     primary = "#B5302A",
     base_font = bslib::font_google("Source Serif Pro"),
-    heading_font = bslib::font_google("Josefin Sans", wght = 100))
+    heading_font = bslib::font_google("Josefin Sans", wght = 100)
+  )
 }
 
 #' 8-color palette
-colors_8 = function() {
-  c("#ba1c21",
+colors_8 <- function() {
+  c(
+    "#ba1c21",
     "#001f82",
     "#61333d",
     "#626370",
     "#1b2442",
     "#0b8581",
     "#7e9e9c",
-    "#2a571d")
+    "#2a571d"
+  )
 }
 
 #' 10-color palette
-colors_10 = function() {
-  c("#ba1c21",
+colors_10 <- function() {
+  c(
+    "#ba1c21",
     "#001f82",
     "#61333d",
     "#626370",
@@ -54,15 +59,18 @@ colors_10 = function() {
     "#7e9e9c",
     "#575978",
     "#2a571d",
-    "#0e3303")
+    "#0e3303"
+  )
 }
 
 #' Value box color palette
-colors_value_boxes = function() {
-  c("#1cb05c",
+colors_value_boxes <- function() {
+  c(
+    "#1cb05c",
     "#bc5b10",
     "#8897be",
     "#9e557f",
     "#aec2b6",
-    "#03518c")
+    "#03518c"
+  )
 }
